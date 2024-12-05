da45f9e5f7795e4fdb81e30ad21e1ed0c7fafff1
Sergeypanikov <svp1967@gmail.com>
2024-12-05 18:32:25 +0300

    Домашнее задание "Условная конструкция. Операторы.
    Не знаю, но почему-тоо обошелся без операторов elif, else

---
+++ module_2_2.py
@@ -0,0 +1,9 @@
+first = 3
+second = 7
+third = 9
+if first == second and second == third: #punkt 1
+    print(3)
+if first == second or second == third: #punkt 2
+    print(2)
+if first != second and second != third and first != third: #punkt 3
+    print(0)
